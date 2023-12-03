55f1e6e5085b8102a4c819d2227fdbf6df5f9fc5
h268081 <h268081@stud.u-szeged.hu>
2023-12-03 14:59:54 +0100

    customMatrix.h added

--- customVectors/CustomMatrix.h
+++ customVectors/CustomMatrix.h
@@ -3,8 +3,23 @@
 //
 
 #pragma once
+#include "CustomVector.h"
 
 class CustomMatrix {
+    CustomVector* mtx;
+    int rows;
+    int coloumns;
+public:
+    explicit CustomMatrix(int=1,int=1);
 
+    CustomMatrix(int*[],int,int);
+
+    CustomMatrix(CustomMatrix&);
+
+    static CustomVector* createNewMtx(int,int);
+
+    static void destroyMtx(const CustomVector*);
+
+    static void cpyMtx()
 };
 
